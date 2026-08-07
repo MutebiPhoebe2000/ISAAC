6578cd59ecd55c5d57f4579cfb814c591ec739bf
PheBanks <mutebiphoebe2000@gmail.com>
2026-08-07 17:03:54 +0300

    Fix SMTP connection by forcing IPv4

--- backend/server/services/email.js
+++ backend/server/services/email.js
@@ -11,6 +11,8 @@ function getTransporter() {
     host: process.env.EMAIL_HOST,
     port: Number(process.env.EMAIL_PORT) || 587,
     secure: Number(process.env.EMAIL_PORT) === 465,
+    requireTLS: Number(process.env.EMAIL_PORT) !== 465,
+    family: 4,
     auth: {
       user: process.env.EMAIL_USER,
       pass: process.env.EMAIL_PASS
